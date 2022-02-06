46ea01b05f627750d018a04586905a198720a876
Zvezdan95 <zvezdann11@gmail.com>
2022-02-06 12:15:52 +0100

    node fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 app
     .get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + 'index.html'));
+        res.sendFile(path.join(__dirname + '/index.html'));
     })
 
     .listen(PORT, () => {
